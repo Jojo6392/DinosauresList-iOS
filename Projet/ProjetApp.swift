4fda5d304b4bc84eb96c7cd1a85a6f1f6f12b3ae
Joris Margotteau <87821711+Jojo6392@users.noreply.github.com>
2022-02-09 15:53:01 +0100

    Initial Commit

---
+++ Projet/ProjetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetApp.swift
+//  Projet
+//
+//  Created by Joris on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
